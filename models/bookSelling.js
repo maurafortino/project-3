5af493096eae70479470c283e9b1cfbe2c117696
maurafortino <maurafortino@gmail.com>
2020-01-05 22:40:40 -0500

    added the models for the database. not set in stone

---
+++ models/bookSelling.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const sellingSchema = new Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    isbn: { type: Number, required: true },
+    published: { type: Number},
+    edition: { type: Number},
+    condition: {type: String, required: true},
+    subject: {type: String, required: true},
+    date: { type: Date, default: Date.now },
+    sold: {type: Boolean, default: false}
+});
+
+const Selling = mongoose.model("Selling", sellingSchema);
+
+module.exports = Selling;
