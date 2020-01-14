96139a67e37ba20abf40c055984efa3cb6b8a89f
Kiylah O'Brien <kiylah2000@gmail.com>
2020-01-13 19:00:16 -0500

    added code to src file for navbar

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import Navbar from './components/navbar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Navbar />, document.getElementById('root'));
 
