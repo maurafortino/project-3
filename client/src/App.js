673a8452c9f20f0991b8979459615f811c4bb6cf
Kiylah O'Brien <kiylah2000@gmail.com>
2020-01-13 20:25:35 -0500

    added more code for the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
