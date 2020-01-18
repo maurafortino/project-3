c6f8f1872866f07fc37d81e37036949ce565a6d1
maurafortino <maurafortino@gmail.com>
2020-01-18 12:35:36 -0600

    added the signup form and add the link to the index form and app js

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import SignUp from "../components/SignUp";
+
+
+class Signup extends Component {
+    render() {
+        return (
+            <div>
+                <SignUp/>
+            </div>
+        )
+    };
+};
+
+export default Signup;
