01fab50a8edd39b610c346536368bcc550d4240d
maurafortino <maurafortino@gmail.com>
2020-01-18 09:41:20 -0600

    login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import SignIn from "../components/Form"
+
+
+class Login extends Component {
+    render() {
+        return (
+            <SignIn />
+        )
+    };
+};
+
+export default Login;
