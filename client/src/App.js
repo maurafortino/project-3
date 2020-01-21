0280474f29eeabbf42c7e4355d264a2132b0b220
Kiylah O'Brien <kiylah2000@gmail.com>
2020-01-20 22:05:21 -0500

    finished the appbar, added a search bar, stared the How it works page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import SearchAppBar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login";
+import HowItWorks from "./pages/HowItWorks/works";
+import HomeSearchbar from './components/Searchbar/HomeSearchbar';
 
 
 function App() {
@@ -12,9 +14,11 @@ function App() {
     <Router>
       <div>
         <SearchAppBar />
+        <HomeSearchbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login}/>
+          <Route exact path="/HowItWorks" component={HowItWorks}/>
         </Switch>
       </div>
     </Router>
---
+++ client/src/pages/HowItWorks/works.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+
+class HowItWorks extends Component {
+    render() {
+        return (
+
+
+            <div className="container-fluid">
+
+                <div className="description">
+                    <p className="lead">
+                        Book Exchange is a website that helps students purchase textbooks books without the expensive price. The service was designed to help students sell textbooks with little to no value and get rid of extra books laying around the house.
+                        Users are able to brows the catelog of books that are offered by title or major and then contact the seller to arrange how to to recieve the book. The only expense that the buyer/user pays is the shipping cost of the book to the new location.
+                        <br></br>
+                        Example:
+                        1. Find a book that you want.
+                        2. Contact the seller and wait for a response.
+                        3. Make arrangemets to ship or meet up to exchange the book depending on the location of the seller/buyer
+                        4.Recieve your book!
+                    </p>
+                </div>
+
+
+            </div>
+        );
+    }
+};
+
+export default HowItWorks;
