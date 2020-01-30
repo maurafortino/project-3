1b688b6b1f70c34a6307e786c8bd3fa9e78f8909
maurafortino <maurafortino@gmail.com>
2020-01-29 23:20:36 -0600

    fixing signup page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -5,7 +5,7 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import API from "../utils/BookAPI";
+import API from "../utils/API";
 import "../pages/signup.css";
 import { Link } from "react-router-dom";
 
