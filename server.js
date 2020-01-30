bfe1ec7d88388060f7a30fa4cb431ca75da1867d
maurafortino <maurafortino@gmail.com>
2020-01-29 22:57:11 -0600

    changed back db connection

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const routes = require("./routes");
-const config = require("config");
-
-const db = config.get("mongoURI");
-
-mongoose.connect(db || "mongodb://localhost/bookexchange");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bookexchange");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
